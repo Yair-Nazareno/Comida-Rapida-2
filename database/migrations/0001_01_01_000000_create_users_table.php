adca829abca8964655e2d9150da33255be2934fa
don naza <ymnazareno@est.istg.edu.ec>
2026-03-28 17:07:13 -0500

    feature/auth-jwt-loginRequests

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('role', 20)->default('user');
+            $table->enum('role', ['admin', 'cajero', 'trabajador'])->default('trabajador');
             $table->string('email')->unique();
+            $table->boolean('active')->default(true);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
